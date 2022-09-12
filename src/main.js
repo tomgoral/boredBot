ad6b74af9d5255746043359e06ff2832d0daa9cd
Tom Goral <tg@tomgoral.com>
2022-09-12 16:25:13 -0400

    vite config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 
 document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
+  fetch("http://www.boredapi.com/api/activity/")
     .then(response => response.json())
     .then(data => {
       document.getElementById("activity").textContent = data.activity
