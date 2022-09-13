ded4b0a00779086687ff1279eff5962579f49ab0
Tom Goral <tg@tomgoral.com>
2022-09-13 11:19:44 -0400

    console log

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ document.getElementById("get-activity").addEventListener("click", function() {
   fetch("http://www.boredapi.com/api/activity/")
     .then(response => response.json())
     .then(data => {
+      console.log(data)
       document.getElementById("activity").textContent = data.activity
     })
 })
